6a63d082c327f759d87657f39abb117122aacf2a
Bas van de Pol <bas.van.de.pol@rabobank.nl>
2018-05-29 22:01:53 +0200

    Service updates

--- server.js
+++ server.js
@@ -20,9 +20,13 @@ MongoClient.connect(db.url, (err, database) => {
     if (err) return console.log(err);
 
     require('./service/routes')(app, database);
+    app.use(express.static(__dirname + '/app/dist/widm-app'));
+    app.get('/*', function(req, res) {
+        res.sendFile(path.join(__dirname + '/app/dist/widm-app/index.html'));
+    });
     app.listen(process.env.PORT || 5000)
 });
 
 
-//app.use( express.static(__dirname + '/app/dist/widm-app/' ) );
-app.use(express.static(path.join(__dirname, '/app/dist/widm-app/')));
+
+
--- service/routes/index.js
+++ service/routes/index.js
@@ -1,6 +1,6 @@
 //const serviceRoutes = require('./service_routes');
-const memberRoutes = require('./member_routes')
-const daysRoutes = require('./days_routes')
+const memberRoutes = require('./member_routes');
+const daysRoutes = require('./days_routes');
 
 module.exports = (app, db) => {
     memberRoutes(app, db);
