90163f189f94fe23fdfc6d9eea24f69d72a55c89
Bas van de Pol <bas.van.de.pol@rabobank.nl>
2018-05-29 22:10:23 +0200

    heroku

--- server.js
+++ server.js
@@ -19,11 +19,14 @@ app.use(cors(corsOptions));
 MongoClient.connect(db.url, (err, database) => {
     if (err) return console.log(err);
 
+    const dirName = __dirname + '/app/dist/widm-app';
     require('./service/routes')(app, database);
-    app.use(express.static(__dirname + '/app/dist/widm-app'));
+
+    app.use(express.static(dirName));
     app.get('/*', function(req, res) {
-        res.sendFile(path.join(__dirname + '/dist/widm-app/index.html'));
+        res.sendFile(path.join(`${dirName}/index.html`));
     });
+
     app.listen(process.env.PORT || 5000)
 });
 
