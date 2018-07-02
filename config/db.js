ea19e4c1ef4f08021e51ed940420d565eda2c59b
Bas van de Pol <bas.van.de.pol@rabobank.nl>
2018-07-02 17:33:23 +0200

    no message

--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url : 'mongodb://bvdpol:test@ds237660.mlab.com:37660/heroku_fbdjrcmw'
+    url : 'mongodb://bvdpol:JtAJC5hCHo=;wwjw>%[yyP@ds225641-a0.mlab.com:25641,ds225641-a1.mlab.com:25641/heroku_cd647bl6?replicaSet=rs-ds225641'
 };
