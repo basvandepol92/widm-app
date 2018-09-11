26a52832e5cd12ef1a5652373d5ca5152ebb0d2a
Bas van de Pol <bas.van.de.pol@rabobank.nl>
2018-09-11 13:40:37 +0200

    Dr. Frankenstein this application to show it to CODEZILLA colleagues tomorrow.

--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url : 'mongodb://bvdpol:EAk4qZpbVYJvoYPEsdFHvs@ds225641-a0.mlab.com:25641,ds225641-a1.mlab.com:25641/heroku_cd647bl6?replicaSet=rs-ds225641'
+    url : 'mongodb://bvdpol:test123@ds147011.mlab.com:47011/heroku_c9qfq3vx'
 };
